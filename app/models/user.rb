ee08e2492f7122beb860785eefbc1eee4762e297
t-kyan <lifeishard-dakara-happy@outlook.jp>
2023-08-08 23:02:06 +0900

    新規登録機能（バリデーション、エラーメッセージの設定）

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,19 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
+
+  with_options presence: true do
+    validates :nickname
+    validates :last_name
+    validates :first_name
+    validates :last_name_kana
+    validates :first_name_kana
+    validates :birth_date
+    validates :email, uniqueness: true,
+                      format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "@を含む必要があります" }
+    validates :password, length: { minimum: 6 },
+                               format: { with: /\A(?=.*[a-zA-Z])(?=.*[0-9])/,
+                                         message: "は半角英数字混合で入力してください" }      
+  end     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-
-  devise_for :users
   get 'items/index'
   root to: "items#index"
+  
+  devise_for :users
+  
 end
