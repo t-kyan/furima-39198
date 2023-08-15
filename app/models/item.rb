7d5f8d728f77971ee9d97d22facfb8bd6edf3b8d
t-kyan <lifeishard-dakara-happy@outlook.jp>
2023-08-15 19:37:03 +0900

    ActiveHashを利用するモデルファイル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,11 @@
 class Item < ApplicationRecord
   has_one_attached :image
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :item_condition
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :shipping_date
+  
 end
