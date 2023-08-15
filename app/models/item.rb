517a3ce7eb08b7986c984b3ce1f4d02286e0be00
t-kyan <lifeishard-dakara-happy@outlook.jp>
2023-08-15 20:08:15 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,31 @@
 class Item < ApplicationRecord
   has_one_attached :image
-
+  belongs_to :user
+  
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :item_condition
   belongs_to :shipping_cost
   belongs_to :prefecture
   belongs_to :shipping_date
-  
+
+  with_options presence: true do
+    validates :image 
+    validates :name
+    validates :description 
+    validates :category_id
+    validates :item_condition_id 
+    validates :shipping_cost_id 
+    validates :prefecture_id
+    validates :shipping_date_id
+    validates :price
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :item_condition_id
+    validates :shipping_cost_id
+    validates :prefecture_id
+    validates :shipping_date_id
+  end
 end
