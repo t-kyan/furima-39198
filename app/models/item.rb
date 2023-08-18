ce642cce9f52d9981cde38ae9a5c88b58ae895d9
t-kyan <lifeishard-dakara-happy@outlook.jp>
2023-08-19 02:36:17 +0900

    商品出品　モデルの単体テスト

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,19 +13,16 @@ class Item < ApplicationRecord
     validates :image 
     validates :name
     validates :description 
-    validates :category_id
-    validates :item_condition_id 
-    validates :shipping_cost_id 
-    validates :prefecture_id
-    validates :shipping_date_id
-    validates :price
   end
 
-  with_options numericality: { other_than: 1 , message: "can't be blank"} do
+  with_options numericality: { other_than: 1 , message: "can't be blank" } do
     validates :category_id
     validates :item_condition_id
     validates :shipping_cost_id
     validates :prefecture_id
     validates :shipping_date_id
   end
+
+  validates :price, presence: true, format: { with:  /\A[0-9]+\z/, message:"is invalid. Input half-width characters" },
+                    numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message:"is out of setting range"}
 end
---
+++ spec/factories/items.rb
@@ -0,0 +1,19 @@
+FactoryBot.define do
+  factory :item do
+    association :user 
+
+    name                { Faker::Name.name }
+    description         { Faker::Lorem.sentence }
+    category            { Category.where.not(id: 1).sample || association(:category) }
+    item_condition      { ItemCondition.where.not(id: 1).sample || association(:item_condition) }
+    shipping_cost       { ShippingCost.where.not(id: 1).sample || association(:shipping_cost) }
+    prefecture          { Prefecture.where.not(id: 1).sample || association(:prefecture) }
+    shipping_date       { ShippingDate.where.not(id: 1).sample || association(:shipping_date) }
+    price               { Faker::Number.between(from: 300, to: 9999999) }
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+      
+    end
+  end
+end
