784e0640d98e0ac36d0b60f7e340cf9eecf4ef27
t-kyan <lifeishard-dakara-happy@outlook.jp>
2023-08-19 18:18:17 +0900

    販売手数料や利益計算結果を表示する

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,7 +1,14 @@
 window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
+  const addTaxDom = document.getElementById("add-tax-price");
+  const profitValue = document.getElementById("profit");
+
   priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value;
-    console.log(inputValue);
-  })
+  const inputValue = priceInput.value; 
+  const taxValue = inputValue * 0.1; // 手数料計算
+  const profit = inputValue * 0.9; // 販売利益計算
+
+  addTaxDom.innerHTML = Math.floor(taxValue).toLocaleString(); // 手数料を整数に変換して表示
+  profitValue.innerHTML = Math.floor(profit).toLocaleString(); // 販売利益を整数に変換して表示
+  });
 });
