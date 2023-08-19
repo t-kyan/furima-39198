1bfbbafd89ad0530f1fb8d95e8e6d943995a3b0c
t-kyan <lifeishard-dakara-happy@outlook.jp>
2023-08-19 18:14:55 +0900

    必要なvalue属性を取得できるようにする

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
-     console.log("イベント発火");
+    const inputValue = priceInput.value;
+    console.log(inputValue);
   })
 });
