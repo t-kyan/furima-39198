080064e4004f77417dcd5f760e523a102fab9133
t-kyan <lifeishard-dakara-happy@outlook.jp>
2023-08-21 15:46:53 +0900

    eachメソッドで一覧表示、if文で表示内容変える実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,8 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
 
   def index
-    @items = Item.all
+    @items = Item.all.order('created_at DESC')
+    @shipping_costs = ShippingCost.all
   end
 
   def new
