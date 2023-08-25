6447ceb34e72c9f64f9459a88dae70df0d27bb53
t-kyan <lifeishard-dakara-happy@outlook.jp>
2023-08-25 15:16:10 +0900

    destroyアクション定義
    set_itemメソッドで記述をまとめました。出品ユーザーのみ商品削除ができるように、条件分岐を追記しました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -36,8 +36,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    if item.destroy
+    if current_user == @item.user
+      @item.destroy
       redirect_to root_path
     end
   end
