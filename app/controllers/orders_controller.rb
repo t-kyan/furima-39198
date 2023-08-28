9e05c791162cdc60022612f48a405351b1e77a36
t-kyan <lifeishard-dakara-happy@outlook.jp>
2023-08-28 19:26:49 +0900

    購入画面で商品情報を表示する実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,23 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :crete]
   def index
-    @order_delivery = OrderDelivery.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    @order_delivery = OrderDelivery.new(order_params)
+    @item = Item.find(params[:item_id])
+    if @order_delivery.valid?
+      @order_delivery.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_delivery).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
